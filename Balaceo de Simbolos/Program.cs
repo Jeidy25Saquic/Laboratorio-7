6c4eb07ab6870ad724bf14a78f18d605985dfec0
Jennifer Saquic <jenntecum2001@gmail.com>
2024-09-23 12:12:09 -0600

    Verifica signos

---
+++ Balaceo de Simbolos/Program.cs
@@ -0,0 +1,82 @@
+﻿class Program()
+{
+     
+
+     public static void Main(string[] args)
+    {  bool continuar=true;
+        do
+        {
+            Console.Clear();
+            Console.WriteLine("Ingrese una secuencia usando [{(  y su respectivo cierre");
+            string secuencia = Console.ReadLine();
+            if (AgregarPila(secuencia) == true)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Esta balaceado");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("No esta balaceado");
+            }
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Desea Agregar otra secuencia [si][no]");
+            Console.ResetColor();
+            string continua= Console.ReadLine();
+            if(continua == "no")
+            {
+                Console.WriteLine("Feliz dia");
+                continuar = false;   
+            }
+            
+        } while (continuar);
+    }
+
+  static bool AgregarPila(string secuencia)
+    {
+        Stack<char> abierto= new Stack<char>();
+        
+
+        foreach (char simbolo in secuencia) {
+
+            if (simbolo =='['|| simbolo=='{'|| simbolo == '(')
+            {
+                abierto.Push(simbolo);
+            }
+            else
+            {
+                if (abierto.Count == 0)
+                {
+                    return false;
+                }
+
+                char ultimo = abierto.Pop();
+                /*f (Pares(ultimo, simbolo)== true)
+                 {
+                     return true;
+                 }
+                 else
+                 {
+                     return false;
+                 }*/
+
+                if(Pares(ultimo, simbolo) != true)
+                {
+                    return false;
+                }
+            }
+          
+
+        }
+
+        return abierto.Count == 0;
+
+    }
+
+    static bool Pares(char abierta, char cierre)
+    {
+        return (abierta == '(' && cierre == ')') ||
+               (abierta == '[' && cierre == ']') ||
+               (abierta == '{' && cierre == '}');
+    }
+}
